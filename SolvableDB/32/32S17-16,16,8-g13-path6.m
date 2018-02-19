51b97b837c40caea7cacba4f252108b1bf553a26
Michael Musty <mjmusty@doob.dartmouth.edu>
2018-02-19 07:52:41 -0500

    doob d32

--- SolvableDB/32/32S17-16,16,8-g13-path6.m
+++ SolvableDB/32/32S17-16,16,8-g13-path6.m
@@ -33,6 +33,12 @@ s`SolvableDBBlocks := {@ PowerSet(IntegerRing()) |
 { IntegerRing() | 25, 27 },
 { IntegerRing() | 31, 32 }
 @};
+s`SolvableDBBelyiMapTiming := 2.220p15;
+s`SolvableDBSanityCheckTiming := 31084.440p15;
+s`SolvableDBLocalSanityCheckTiming := 3.650p15;
+s`SolvableDBLocalSanityCheckPrime := 8736028057;
+s`SolvableDBIsLowGenusOrHyperelliptic := false;
+s`SolvableDBIsRamifiedAtEveryLevel := true;
 
 /*
 Permutations and Passports
@@ -117,6 +123,20 @@ s`SolvableDBPointedAutomorphismGroup := PermutationGroup<32 |
 Top Level Belyi Curve/Map
 */
 
+K := Rationals();
+PX<x1, x2, x3, x4> := PolynomialRing(K, 4);
+AA<x1, x2, x3, x4> := AffineSpace(PX);
+I<x1, x2, x3, x4> := ideal< PX | [
+x1^2*x4^2 - x1*x2*x3 - x2*x4^2,
+x2^2*x4^2 - 2*x1*x3 - x4^2,
+x2^3 - 2*x1^2 + x2,
+x1*x3^2 - 1/2*x2^2 + 1/2,
+x3*x4^2 - 1
+] >;
+X<x1, x2, x3, x4> := Curve(AA, I);
+KX<x1, x2, x3, x4> := FunctionField(X);
+s`SolvableDBBelyiCurve := X;
+s`SolvableDBBelyiMap := KX!(1/(2*x3^4)*x2^3 - x2^2 - 3/(2*x3^4)*x2);
 
 /*
 Graph Data
