90789876f874f83f04dbec52938b37aceb72dcd8
michaelmusty <michaelmusty@gmail.com>
2018-08-27 16:08:31 -0400

    point search over number field

--- Code/point_counting.m
+++ Code/point_counting.m
@@ -40,27 +40,68 @@ intrinsic ReduceCurve(X::Crv, p::RngIntElt) -> Crv
 end intrinsic;
 
 intrinsic NaivePointSearch(X::Crv, p::RngIntElt : m := 0) -> Any
-  {Naive point (projective) point count in GF(p^m)}
+  {Naive point (projective) point count in GF(p)}
   if not IsProjective(X) then
     Z := ProjectiveClosure(X);
   else
     Z := X;
   end if;
-  Xp := ReduceCurve(Z, p);
-  if m ne 0 then
-    Xp := BaseChange(Xp, GF(p^m));
-  end if;
+  Xqq := CoerceCurve(X, p : m := m);
+  FFqq<eta> := BaseField(Xqq);
+  vprintf Solvable : "Point search over %o\n", FFqq;
   all_points := []; // projective points
-  for i := 1 to Dimension(Ambient(Xp)) do
-    Xi := AffinePatch(Xp, i);
+  for i := 1 to Dimension(Ambient(Xqq)) do
+    Xi := AffinePatch(Xqq, i);
+    t0 := Cputime();
     ptsi := RationalPoints(Xi);
+    t1 := Cputime();
+    vprintf Solvable : "\n";
+    vprintf Solvable : "# of points in affine patch %o = %o:\n", i, #ptsi;
+    vprintf Solvable : "%o\n", ptsi;
+    vprintf Solvable : "%o seconds\n", t1-t0;
     for pt in ptsi do
       Append(~all_points, ProjectiveRepresentative(pt));
     end for;
   end for;
   all_points_set := SequenceToSet(all_points);
-  vprintf Solvable : "#all_points = %o:\n", #all_points;
-  vprint Solvable : all_points;
-  vprintf Solvable : "#all_points_set = %o:\n", #all_points_set;
-  return SetToSequence(all_points_set);
+  vprintf Solvable : "\nCollecting projective points over %o\n", FFqq;
+  vprintf Solvable : "# projective points = %o:\n", #all_points_set;
+  all_pts := SetToSequence(all_points_set);
+  for pt in all_pts do
+    vprintf Solvable : "%o\n", pt;
+  end for;
+  return all_pts;
+end intrinsic;
+
+intrinsic CoerceCurve(X::Crv, p::RngIntElt : m := 0) -> Crv
+  {Reduce X mod pp and coerce over GF(pp^m)}
+  if not IsProjective(X) then
+    X := ProjectiveClosure(X);
+  end if;
+  K := BaseField(X);
+  charK := Characteristic(K);
+  if charK eq 0 then
+    Xpp := ReduceCurve(X, p);
+    if m gt 1 then
+      FFpp := BaseField(Xpp);
+      FFqq, mp := ext<FFpp|m>;
+      Ipp := Ideal(Xpp);
+      equations_pp := Basis(Ipp);
+      equations_qq := [];
+      grading := Grading(Ipp);
+      P := PolynomialRing(FFqq, grading);
+      for eqn_pp in equations_pp do
+        eqn_qq := PolynomialReduction(eqn_pp, mp, P);
+        Append(~equations_qq, eqn_qq);
+      end for;
+      Iqq := ideal<P|equations_qq>;
+      PP := ProjectiveSpace(Generic(Iqq));
+      Xqq := Curve(PP, Iqq);
+      return Xqq;
+    else
+      return Xpp;
+    end if;
+  else // charK gt 0
+    error "not implemented for finite fields yet >_<\n";
+  end if;
 end intrinsic;
